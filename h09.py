a3ad6b719f92572c97d321b4c95c3900eedc5dc3
kvarbola <kvarbola@hkhk.edu.ee>
2026-01-13 09:50:11 +0200

    push

--- h09.py
+++ h09.py
@@ -40,11 +40,79 @@
 
 # Harjutus 9.5
 
-esimene = True
+#esimene = True
 
-for i in range(200, 321):
-    if i % 7 == 0 and arv % 5 != 0:
-        if not esimene:
-            print(", ", end="")
-        print(arv, end="")
-        esimene = False
+#for i in range(200, 321):
+#    if i % 7 == 0 and arv % 5 != 0:
+#        if not esimene:
+#            print(", ", end="")
+#        print(arv, end="")
+#        esimene = False
+
+# Harjutus 9.8
+
+#for i in range(11):
+#    print(f"{i} * {i} = {i*i}")
+
+# Harjutus 9.9 , 9.10
+
+# import random
+# tehe = ["+","-","*","/"]
+# p = 0
+# kysimuste_arv = 1
+# 
+# for _ in range(kysimuste_arv):
+#     arv1 = random.randint(1,10)
+#     arv2 = random.randint(1,10)
+#     t = random.choice(tehe)
+#     
+#     if t=="+":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("vastus: "))
+#         if arv1+arv2 == v:
+#             p+=1
+#     elif t=="-":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("vastus: "))
+#         if arv1-arv2 == v:
+#             p+=1
+#     elif t=="*":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("vastus: "))
+#         if arv1*arv2 == v:
+#             p+=1
+#     else:
+#         print(f"{arv1}{t}{arv2}=")
+#         v = float(input("vastus: "))
+#         if arv1/arv2 == v:
+#             p+=1
+# print(p)
+# if p/kysimuste_arv >= 0.5:
+#     print(f"A - {p}/{kysimuste_arv}")
+# else:
+#     print("MA - {p}/{kysimuste_arv}")
+
+#Harjutus 9.11
+
+#1
+arv = 5
+for i in range(1,6):
+    print(" "*i+ "*" * arv)
+    arv-=1
+
+#2
+arv = 5
+for i in range(1,6):
+    print("*" * arv)
+    arv-=1
+
+#3
+arv = 5
+for i in range(1,6):
+    print(" " * arv + "*" * i)
+    arv-=1
+
+#4
+arv = 5
+for i in range(1,6):
+    print("*" * i)
