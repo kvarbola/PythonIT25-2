99a0afe845ddec17e6154d03df1ec9b501f581c6
kvarbola <kvarbola@hkhk.edu.ee>
2026-02-10 20:23:21 +0200

    push

--- Python_is_too_nr4/Python_is_too_nr4.5.py
+++ Python_is_too_nr4/Python_is_too_nr4.5.py
@@ -5,6 +5,12 @@ fail = open("mündid.txt", encoding="UTF-8")
 summa = 0
 
 for rida in fail:
-    print(rida)
+    if rida == 1:
+        summa+=int(rida)
+    elif rida == 2:
+        summa+=int(rida)
+    elif rida == 5:
+        summa+=int(rida)
+print(f"Siin on summa: {summa}")
     
 
