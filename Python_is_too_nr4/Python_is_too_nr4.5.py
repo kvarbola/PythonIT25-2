f7c7c4d83ed90f37461afbbe69758969caa12d02
kvarbola <kvarbola@hkhk.edu.ee>
2026-02-11 17:49:08 +0200

    push

--- Python_is_too_nr4/Python_is_too_nr4.5.py
+++ Python_is_too_nr4/Python_is_too_nr4.5.py
@@ -1,12 +1,16 @@
 # 4.5 mündid
 
 nimi = input("Sisesta failinimi: ")
+summa = 0
 
 def pronksikarva_summa(a):
+    global summa
+    
     fail = open("mündid.txt", encoding="UTF-8")
     
     for rida in fail:
-        print(rida)
-    return "lõpp"
+        if int(rida) <= 5:
+            summa += int(rida)
+    print(summa)
 
 pronksikarva_summa(nimi)
--- Python_is_too_nr4/Python_is_too_nr4.6.py
+++ Python_is_too_nr4/Python_is_too_nr4.6.py
@@ -1,4 +1,13 @@
-import datetime
+# 4.6
 
-print(datetime.datetime.year())
+kuupaev = str(input("Sisesta kuupäev formaadis DD.MM.YYYY: "))
 
+def kuu_sonana(a):
+    
+    kuud = ["Vali kuu","jaanuar","veebruar","märts","april","mai","juuni","juuli","august","september","oktoober","november","detsember"]
+
+    x = kuupaev.split(".")
+
+    print(f"{int(x[0])}.{kuud[int(x[1])]} {int(x[2])}")
+    
+kuu_sonana(kuupaev)
